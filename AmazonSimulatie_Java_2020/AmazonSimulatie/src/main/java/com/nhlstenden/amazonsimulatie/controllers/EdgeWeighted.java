df2df2023a2f226f6e5c4f8ce385b765976b5577
Rienk <rienk2001@hotmail.com>
2020-10-27 10:39:06 +0100

    DijsktraAlgortime

--- AmazonSimulatie_Java_2020/AmazonSimulatie/src/main/java/com/nhlstenden/amazonsimulatie/controllers/EdgeWeighted.java
+++ AmazonSimulatie_Java_2020/AmazonSimulatie/src/main/java/com/nhlstenden/amazonsimulatie/controllers/EdgeWeighted.java
@@ -5,8 +5,9 @@ public class EdgeWeighted implements Comparable<EdgeWeighted> {
     NodeWeighted source;
     NodeWeighted destination;
     double weight;
+    Character coordinate;
 
-    EdgeWeighted(NodeWeighted s, NodeWeighted d, double w) {
+    EdgeWeighted(NodeWeighted s, NodeWeighted d, double w, Character c) {
         // Note that we are choosing to use the (exact) same objects in the Edge class
         // and in the GraphShow and GraphWeighted classes on purpose - this MIGHT NOT
         // be something you want to do in your own code, but for sake of readability
@@ -14,6 +15,7 @@ public class EdgeWeighted implements Comparable<EdgeWeighted> {
         source = s;
         destination = d;
         weight = w;
+        coordinate = c;
     }
 
     // ...
